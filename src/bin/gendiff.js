e72d1f14a39567e91071340f50d80d8b3f8012cc
altuninovich@gmail.ru <root@DESKTOP-EFJ8J1R.localdomain>
2020-05-31 19:30:56 +0300

    step-2 end

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 import program from 'commander';
-const f = (num) => num;
-
+import genDiff from '../index';
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => (
+    console.log(genDiff(filepath1, filepath2))))
   .parse(process.argv);
-  console.log('hulo');
+  
---
+++ src/index.js
@@ -0,0 +1,24 @@
+const genDiff = (pathToFile1, pathToFile2) => {
+	const dataJson1 = JSON.parse(fs.readFileSync(pathToFile1, 'utf-8'));
+	const dataJson2 = JSON.parse(fs.readFileSync(pathToFile2, 'utf-8'));
+	const arrKey = Object.keys({ ...dataJson1, ...dataJson2});
+
+  const checkingValues = (key) => {
+  if (dataJson1[key] === dataJson2[key]) {
+   return [` ${key}: ${dataJson2[key]}`];
+  } 
+  return [`-${key}: ${dataJson1[key]}`, `+${key}: ${dataJson2[key]}`];
+};
+
+  return arrKey.reduce((acc, key) => {
+    if (dataJson1.hasOwnProperty(key) && !dataJson2.hasOwnProperty(key)) {
+      return [...acc, `-${key}: ${dataJson1[key]}`]
+    }
+    if (!dataJson1.hasOwnProperty(key) && dataJson2.hasOwnProperty(key)) {
+      return [...acc, `+${key}: ${dataJson1[key]}`]
+    }
+    return [...acc, ...checkingValues(key)]
+  }, []).join('\n');
+};
+export default genDiff;
+
