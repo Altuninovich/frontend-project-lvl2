9920837c1bfb31bbd50d200ef96e0a527a5e5363
altuninovich@gmail.ru <root@DESKTOP-EFJ8J1R.localdomain>
2020-06-14 14:28:40 +0300

    linter

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -1,15 +1,14 @@
-import genDiff from '../src/index.js';
 import fs from 'fs';
 import path from 'path';
+import genDiff from '../src/index.js';
 
 const getFixturePathFlat = (filename) => path.join(__dirname, '..', '__fixtures__', 'flat', filename);
 const getFixturePathNested = (filename) => path.join(__dirname, '..', '__fixtures__', 'nested', filename);
-//const readFile = (filename) => JSON.parse(fs.readFile(getFixturePath(filename), 'utf-8'));
 const formats = ['json', 'yml', 'ini'];
 const formatsNested = ['json', 'yml', 'ini'];
 let expected;
 let expectedNestedStylish;
-let expectedNestedPlain
+let expectedNestedPlain;
 
 beforeAll(() => {
   expected = fs.readFileSync(getFixturePathFlat('result.diff'), 'utf-8');
@@ -17,18 +16,18 @@ beforeAll(() => {
   expectedNestedPlain = fs.readFileSync(getFixturePathNested('result-plain.diff'), 'utf-8');
 });
 
- test.each(formats)('compare two %s files', (format) => {
- 	  const before = getFixturePathFlat(`before.${format}`);
-      const after = getFixturePathFlat(`after.${format}`);
-      expect(genDiff(before, after)).toString(expected);
-   });
- test.each(formatsNested)('compare two %s files', (format) => {
- 	  const before = getFixturePathNested(`before.${format}`);
-      const after = getFixturePathNested(`after.${format}`);
-      expect(genDiff(before, after)).toString(expectedNestedStylish);
-   });
- test.each(formatsNested)('compare two %s files', (format) => {
-    const before = getFixturePathNested(`before.${format}`);
-      const after = getFixturePathNested(`after.${format}`);
-      expect(genDiff(before, after, 'plain')).toString(expectedNestedPlain);
-   });
+test.each(formats)('compare two %s files', (format) => {
+  const before = getFixturePathFlat(`before.${format}`);
+  const after = getFixturePathFlat(`after.${format}`);
+  expect(genDiff(before, after)).toString(expected);
+});
+test.each(formatsNested)('compare two %s files', (format) => {
+  const before = getFixturePathNested(`before.${format}`);
+  const after = getFixturePathNested(`after.${format}`);
+  expect(genDiff(before, after)).toString(expectedNestedStylish);
+});
+test.each(formatsNested)('compare two %s files', (format) => {
+  const before = getFixturePathNested(`before.${format}`);
+  const after = getFixturePathNested(`after.${format}`);
+  expect(genDiff(before, after, 'plain')).toString(expectedNestedPlain);
+});
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import program from 'commander';
 import genDiff from '../index';
+
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference')
@@ -9,4 +10,3 @@ program
   .action((filepath1, filepath2) => (
     console.log(genDiff(filepath1, filepath2, program.format))))
   .parse(process.argv);
-  
